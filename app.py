d77ee1317af60c1edb968ce66d775cdb2391edc8
knizam2024 <112634557+knizam2024@users.noreply.github.com>
2025-05-13 23:31:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "SekoBot is running!"
+
+async def get_chat_id(update, context):
+    chat = update.effective_chat
+    print(f"Group name: {chat.title}, ID: {chat.id}")
+
+app = ApplicationBuilder().token("7887402540:AAFQkdijjgFu-II1Of_JZfN34XfxVhGQXB4").build()
+app.add_handler(MessageHandler(filters.ALL, get_chat_id))
+app.run_polling()
+    
